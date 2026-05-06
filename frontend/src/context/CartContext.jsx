79f72826278e8b9a796cf17613b0839e6c4da8eb
zainab <zainab.buildup@gmail.com>
2026-05-06 10:56:13 +0530

    Fix loading state bug using async/await with finally

---
+++ frontend/src/context/CartContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  // Add to cart
+  const addToCart = (product) => {
+    const exists = cart.find((item) => item._id === product._id);
+
+    if (exists) {
+      setCart(
+        cart.map((item) =>
+          item._id === product._id
+            ? { ...item, qty: item.qty + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, qty: 1 }]);
+    }
+  };
+
+  // Remove item
+  const removeFromCart = (id) => {
+    setCart(cart.filter((item) => item._id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+// Custom hook
+export const useCart = () => useContext(CartContext);
